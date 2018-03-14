830c7934b60635f471ea1780cb5aeb1c892a86fb
Curzy <cms9597@gmail.com>
2018-03-15 03:47:55 +0900

    Seperate Coinone API wrapper from App

--- src/App.js
+++ src/App.js
@@ -1,66 +1,28 @@
 import React, { Component } from 'react';
-import crypto from 'crypto';
-import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import CoinoneAPIv2 from './coinone'
 
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      amount: 0.00,
+      limitPrice: 0.00,
+      totalPrice: 0.00,
+    }
 
-    this.getEncodedPayload = this.getEncodedPayload.bind(this);
-    this.getSignature = this.getSignature.bind(this)
-    this.getResponse = this.getResponse.bind(this)
-    this.getAccountBalance = this.getAccountBalance.bind(this);
-  }
-
-  getEncodedPayload(payload) {
-    return new Buffer(JSON.stringify(payload)).toString('base64');
-  }
-
-  getSignature(encodedPayload, secretKey) {
-    return crypto
-      .createHmac('sha512', secretKey.toUpperCase())
-      .update(encodedPayload)
-      .digest('hex');
-  }
-
-  getResponse(url, payload) {
-    const encodedPayload = this.getEncodedPayload(payload);
-    const signature = this.getSignature(encodedPayload, process.env.REACT_APP_SECRET_KEY);
-
-    const headers = {
-      'content-type': 'application/json',
-      'X-COINONE-PAYLOAD': encodedPayload,
-      'X-COINONE-SIGNATURE': signature,
-    };
-
-    const options = {
-      headers: headers
-    };
-
-    axios.post(url, encodedPayload, options)
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  getAccountBalance() {
-    const url = 'https://api.coinone.co.kr/v2/account/balance/';
-    const payload = {
-      'access_token': process.env.REACT_APP_ACCESS_TOKEN,
-      'nonce': Date.now()
-    };
-
-    this.getResponse(url, payload);
+    this.coinone = new CoinoneAPIv2(process.env.REACT_APP_ACCESS_TOKEN, process.env.REACT_APP_SECRET_KEY);
+    
+    this.limitBuy = this.coinone.limitBuy.bind(this);
+    this.limitBuy = this.coinone.limitSell.bind(this);
   }
 
   componentDidMount() {
-    this.getAccountBalance();
+    this.coinone.getAccountBalance();
+    this.coinone.limitBuy(10000, 0.1, 'btc');
+    this.coinone.limitSell(10000000, 0.1, 'btc');
   }
 
   render() {
@@ -73,6 +35,20 @@ class App extends Component {
         <p className="App-intro">
           To get started, Get your personal API Key from <a href="https://coinone.co.kr/developer/app/" target="_blank" rel="noopener noreferrer">Coinone</a> and set your <strong>ACCESS_TOKEN</strong> and <strong>SECRET_KEY</strong> to <code>.env</code>
         </p>
+        <div className='order-section'>
+          <div className='buy-sell-flag'>
+            <button>BUY</button>
+            <button>SELL</button>
+          </div>
+          <div className='order-input'>
+            BTC: <input default={this.state.amount}></input>
+            KRW: <input default={this.state.limitPrice}></input>
+          </div>
+          <div>
+            <p>총 <span className='total-price'>{this.state.totalPrice}</span>원</p>
+            <button>주문 확인</button>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/coinone.js
@@ -0,0 +1,103 @@
+import crypto from 'crypto';
+import axios from 'axios';
+
+
+class CoinoneAPIv2 {
+  constructor(accessToken, secretKey) {
+    this.accessToken = accessToken;
+    this.secretKey = secretKey;
+    this.url = 'https://api.coinone.co.kr/v2';
+    this.payload = {
+      'access_token': accessToken,
+      'nonce': Date.now()
+    }
+  }
+
+  getEncodedPayload(payload) {
+    return new Buffer(JSON.stringify(payload)).toString('base64');
+  }
+
+  getSignature(encodedPayload, secretKey) {
+    return crypto
+      .createHmac('sha512', secretKey.toUpperCase())
+      .update(encodedPayload)
+      .digest('hex');
+  }
+
+  getResponse(url, params) {
+    const payload = Object.assign({}, this.payload, params)
+    const encodedPayload = this.getEncodedPayload(payload);
+    const signature = this.getSignature(encodedPayload, this.secretKey);
+
+    const headers = {
+      'content-type': 'application/json',
+      'X-COINONE-PAYLOAD': encodedPayload,
+      'X-COINONE-SIGNATURE': signature,
+    };
+
+    const options = {
+      headers: headers
+    };
+
+    axios.post(url, encodedPayload, options)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  getAccountBalance() {
+    const url = this.url + '/account/balance/';
+
+    this.getResponse(url, {});
+  }
+
+  limitBuy(price, qty, currency) {
+    const url = this.url + '/order/limit_buy/';
+    const params = {
+      'price': price,
+      'qty': qty,
+      'currency': currency,
+    }
+
+    this.getResponse(url, params);
+  }
+
+  limitSell(price, qty, currency) {
+    const url = this.url + '/order/limit_sell/';
+    const params = {
+      'price': price,
+      'qty': qty,
+      'currency': currency,
+    }
+
+    this.getResponse(url, params);
+  }
+  
+  cancelOrder(orderId, price, qty, sellFlag, currency) {
+    const url = this.url + '/order/cancel/';
+    const params = {
+      'order_id': orderId,
+      'price': price,
+      'qty': qty,
+      'is_ask': sellFlag, // 1 is sell
+      'currency': currency
+    }
+
+    this.getResponse(url, params);
+  }
+
+  orderInfo(orderId, currency) {
+    const url = this.url + '/order/order_info/';
+    const params = {
+      'order_id': orderId,
+      'currency': currency
+    }
+
+    this.getResponse(url, params);
+  }
+};
+
+export default CoinoneAPIv2;
