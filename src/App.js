81ae461fb9293ab53258020b2fbb3cf459796d3a
Curzy <cms9597@gmail.com>
2018-03-17 05:35:12 +0900

    API functions now return Promise object
    - add ticker API(version 1)
    - shows user infomations

--- src/App.js
+++ src/App.js
@@ -1,40 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CoinoneAPIv2 from './coinone'
+import CoinoneAPI from './coinone';
 
 
 class App extends Component {
   constructor(props) {
     super(props);
+    
     this.state = {
       amount: 0.00,
       limitPrice: 0.00,
       totalPrice: 0.00,
+      ticker: {
+        btc: {}
+      },
+      userInfo: {},
+      balance: {},
+      rendered: false,
     }
 
-    this.coinone = new CoinoneAPIv2(process.env.REACT_APP_ACCESS_TOKEN, process.env.REACT_APP_SECRET_KEY);
-    
-    this.limitBuy = this.coinone.limitBuy.bind(this);
-    this.limitBuy = this.coinone.limitSell.bind(this);
+    this.coinone = new CoinoneAPI(process.env.REACT_APP_ACCESS_TOKEN, process.env.REACT_APP_SECRET_KEY);
   }
 
   componentDidMount() {
-    this.coinone.getAccountBalance();
-    this.coinone.limitBuy(10000, 0.1, 'btc');
-    this.coinone.limitSell(10000000, 0.1, 'btc');
+    this.getBaseInformations(this.coinone)
+    this.setState({rendered: true})
   }
 
   render() {
+    const balance = !this.isEmptyObject(this.state.balance) ? (
+      <div className='balance'>
+        <p className='balance-btc'>BTC / 거래 가능: {this.state.balance.btc.avail} 자산: {this.state.balance.btc.balance}</p>
+        <p className='balance-krw'>KRW / 거래 가능: {this.state.balance.krw.avail} 자산: {this.state.balance.krw.balance}</p>
+      </div>
+    ) : (
+      <div className='balance no-balance'>
+        지갑 정보를 불러올 수 없습니다
+      </div>
+    );
+
+    const btcTicker = !this.isEmptyObject(this.state.ticker.btc) ? this.formatMoney(this.state.ticker.btc.last) : 'X'
+
+    const welcome = !this.isEmptyObject(this.state.userInfo) ? (
+      <p className='App-intro'>환영합니다! {this.state.userInfo.mobileInfo.userName}님</p> 
+    ) : (
+      <p className='App-intro need-cert'>코인원에서 인증을 마쳐야 합니다</p> 
+    )
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Coinone Easy Trade</h1>
         </header>
-        <p className="App-intro">
-          To get started, Get your personal API Key from <a href="https://coinone.co.kr/developer/app/" target="_blank" rel="noopener noreferrer">Coinone</a> and set your <strong>ACCESS_TOKEN</strong> and <strong>SECRET_KEY</strong> to <code>.env</code>
-        </p>
+        {welcome}
+        {balance}
+        <div className='btc-ticker'>
+          <p>비트코인/KRW {btcTicker}원</p>
+        </div>
         <div className='order-section'>
           <div className='buy-sell-flag'>
             <button>BUY</button>
@@ -51,6 +74,35 @@ class App extends Component {
         </div>
       </div>
     );
+  }
+
+  getBaseInformations(coinone) {
+    const ticker = coinone.ticker('btc');
+    const userInfo = coinone.userInfo();
+    const balance = coinone.accountBalance();
+    Promise.all([ticker, userInfo, balance])
+      .then(result => {
+        this.setState({
+          ticker: {
+            btc: result[0]
+          },
+          userInfo: result[1].userInfo,
+          balance: result[2]
+        })
+      })
+  }
+
+  formatMoney(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  isEmptyObject(obj) {
+    for(const key in obj) {
+      if(obj.hasOwnProperty(key)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
 
--- src/coinone.js
+++ src/coinone.js
@@ -2,16 +2,18 @@ import crypto from 'crypto';
 import axios from 'axios';
 
 
-class CoinoneAPIv2 {
-  constructor(accessToken, secretKey) {
+class CoinoneAPI {
+  constructor(accessToken, secretKey, version = 'v2') {
     this.accessToken = accessToken;
     this.secretKey = secretKey;
+    this.version = version;
     this.proxy = 'https://thingproxy.freeboard.io/fetch/';
-    this.url = this.proxy + 'https://api.coinone.co.kr/v2';
+    this.url = this.proxy + 'https://api.coinone.co.kr/' + this.version;
     this.payload = {
       'access_token': accessToken,
       'nonce': Date.now()
-    }
+    };
+
   }
 
   getEncodedPayload(payload) {
@@ -25,34 +27,56 @@ class CoinoneAPIv2 {
       .digest('hex');
   }
 
-  getResponse(url, params) {
+  async v2Post(endpoint, params) {
     const payload = Object.assign({}, this.payload, params)
     const encodedPayload = this.getEncodedPayload(payload);
     const signature = this.getSignature(encodedPayload, this.secretKey);
 
     const headers = {
-      'content-type': 'application/json',
       'X-COINONE-PAYLOAD': encodedPayload,
       'X-COINONE-SIGNATURE': signature,
+      'content-type': 'application/json',
+      'accept': 'application/json',
     };
 
     const options = {
-      headers: headers
+      headers: headers,
+      timeout: 5000,
     };
 
-    axios.post(url, encodedPayload, options)
+    const result = await axios.post(endpoint, encodedPayload, options)
       .then(response => {
-        console.log(response);
+        return response.data;
       })
       .catch(error => {
-        console.log(error);
+        throw error;
       });
+
+    return result
   }
 
-  getAccountBalance() {
+  async v1Get(endpoint, params) {
+    const result = await axios.get(endpoint, { params: params })
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        throw error;
+      });
+
+    return result
+  }
+
+  accountBalance() {
     const url = this.url + '/account/balance/';
 
-    this.getResponse(url, {});
+    return this.v2Post(url, {});
+  }
+
+  userInfo() {
+    const url = this.url + '/account/user_info/';
+
+    return this.v2Post(url, {});
   }
 
   limitBuy(price, qty, currency) {
@@ -63,7 +87,7 @@ class CoinoneAPIv2 {
       'currency': currency,
     }
 
-    this.getResponse(url, params);
+    return this.v2Post(url, params);
   }
 
   limitSell(price, qty, currency) {
@@ -74,7 +98,7 @@ class CoinoneAPIv2 {
       'currency': currency,
     }
 
-    this.getResponse(url, params);
+    return this.v2Post(url, params);
   }
   
   cancelOrder(orderId, price, qty, sellFlag, currency) {
@@ -87,18 +111,46 @@ class CoinoneAPIv2 {
       'currency': currency
     }
 
-    this.getResponse(url, params);
+    return this.v2Post(url, params);
+  }
+
+  completeOrders(currency) {
+    const url = this.url + '/order/complete_orders/'
+    const params = {
+      'currency': currency,
+    }
+
+    return this.v2Post(url, { params: params });
+  }
+
+  limitOrders(currency) {
+    const url = this.url + '/order/limit_orders/'
+    const params = {
+      'currency': currency,
+    }
+
+    return this.v2Post(url, params);
   }
 
   orderInfo(orderId, currency) {
     const url = this.url + '/order/order_info/';
     const params = {
       'order_id': orderId,
+      'currency': currency,
+    }
+
+    return this.v2Post(url, params);
+  }
+
+  ticker(currency) {
+    const url = this.url.slice(0, -3) + '/ticker/';
+    const params = {
       'currency': currency
     }
 
-    this.getResponse(url, params);
+    return this.v1Get(url, params);
   }
+
 };
 
-export default CoinoneAPIv2;
+export default CoinoneAPI;
